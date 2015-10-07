13253d31aa0ce862cc2aef109774e72fb342926a
abhiabhi15 <abhishek.agrawal15@yahoo.com>
2015-10-07 14:40:48 -0400

    Adding simple track word base tweet api

--- tweets/test/word_tweet_test.js
+++ tweets/test/word_tweet_test.js
@@ -1,5 +1,5 @@
 var twitter = require('ntwitter');
-
+ 
 var t = new twitter({
      consumer_key : process.env.TWITTER_CONSUMER_KEY,
      consumer_secret : process.env.TWITTER_CONSUMER_SECRET,
@@ -8,15 +8,15 @@ var t = new twitter({
 });
  
 // Words or hashtags for tracking tweets
-var words = ['DataScience','BigData','DataScientist','Analytics','Python','MachineLearning','Statistics','rstats','DataMining','DataAnalytics','DataAnalysis','PredictiveAnalytics','SmartCity','IOT','hadoop','dataviz','DeepLearning','BusinessIntelligence']; 
-
+var words = ['Election','Obama']; 
 console.log("Tweets streaming for words : [" + words  + "] ");
-t.stream(
-   	'statuses/filter',
-    { track: words, locations:'35.872341, 35.693292,-78.910483,-78.439650' },
-   	function(stream) {
-       	stream.on('data', function(tweet) {
-			console.log(tweet.text + " ---- User = " + tweet.user.name);
-    	});
-    }
-);
+
+ 
+  t.stream(
+    'statuses/filter',
+    { track: words },
+    function(stream) {
+        stream.on('data', function(tweet) {
+				console.log(tweet.text);
+        });
+    });
