baa7425dde7cd803cb476f6bf227c662768a502b
abhiabhi15 <abhishek.agrawal15@yahoo.com>
2015-10-07 14:20:40 -0400

    Re-arranging files and projects

--- sentiment-analysis/debugFunctions.R
+++
@@ -1,7 +0,0 @@
-my_preprocess <- function(counts){
-  tweets <- getMongoTweets(counts)
-  tweets <- VCorpus(VectorSource(tweets))
-  inspect(tweets)
-  tweets <- preprocess(tweets)
-  inspect(tweets)
-}
